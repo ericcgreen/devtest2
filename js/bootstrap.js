3a0e461b9f785d20a91e954c84283ec3f7c1110a
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-06-29 16:35:52 -0400

    added more functionality and styling

---
+++ js/bootstrap.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+  // Closes the sidebar menu
+  $("#menu-close").click(function(e) {
+      e.preventDefault();
+      $("#sidebar-wrapper").toggleClass("active");
+  });
+  // Opens the sidebar menu
+  $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $("#sidebar-wrapper").toggleClass("active");
+  });
+  // Scrolls to the selected menu item on the page
+  $(function() {
+      $('a[href*=#]:not([href=#],[data-toggle],[data-target],[data-slide])').click(function() {
+          if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+              if (target.length) {
+                  $('html,body').animate({
+                      scrollTop: target.offset().top
+                  }, 1000);
+                  return false;
+              }
+          }
+      });
+  });
+  //#to-top button appears after scrolling
+  var fixed = false;
+  $(document).scroll(function() {
+      if ($(this).scrollTop() > 250) {
+          if (!fixed) {
+              fixed = true;
+              // $('#to-top').css({position:'fixed', display:'block'});
+              $('#to-top').show("slow", function() {
+                  $('#to-top').css({
+                      position: 'fixed',
+                      display: 'block'
+                  });
+              });
+          }
+      } else {
+          if (fixed) {
+              fixed = false;
+              $('#to-top').hide("slow", function() {
+                  $('#to-top').css({
+                      display: 'none'
+                  });
+              });
+          }
+      }
+  });
+  // Email/Sign-up verification
+  $("#submit-btn").click(function() {
+    var validRegExp = /^[^@]+@[^@]+.[a-z]{2,}$/i;
+    var email = $("#inputEmail").val(); // Store E-mail input value in the variable email.
+    var zipcode = $("#inputZipcode").val(); // Store Password input value in the variable password.
+    /* Check if email has regular expressions to display message */
+    if (email.search(validRegExp) == -1) {
+      $("#message").html("i find your lack of faith disturbing.");
+      $("input").val("");
+    } else {
+      $("#message").html("together we can rule the galaxy!!!");
+      $("input").val("");
+    }
+  });
+});
